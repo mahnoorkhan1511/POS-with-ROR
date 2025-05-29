938879e8940d1d24dd40e31e5322037a9a9d38b1
mahnoorkhan1511 <mahnoor1511@gmail.com>
2025-05-29 12:09:19 +0500

    removed some other  migration

--- db/migrate/20250527130859_remove_orders_foriegn_keys.rb
+++
@@ -1,6 +0,0 @@
-class RemoveOrdersForiegnKeys < ActiveRecord::Migration[7.2]
-  def change
-    remove_foreign_key :ordered_products, :orders
-    remove_foreign_key :transactions, :orders
-  end
-end
--- db/migrate/20250527131856_add_back_foreign_keys.rb
+++
@@ -1,6 +0,0 @@
-class AddBackForeignKeys < ActiveRecord::Migration[7.2]
-  def change
-    add_foreign_key :ordered_products, :orders
-    add_foreign_key :transactions, :orders
-  end
-end
